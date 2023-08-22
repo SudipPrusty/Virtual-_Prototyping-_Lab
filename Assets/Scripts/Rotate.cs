1ea761ebb30f4f10cb3796ab5e2735a94c63f03b
Chiranjeeb Garnayak <garnayakchiranjeeb@gmail.com>
2023-08-22 18:12:36 +0530

    Changes

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+
+
+    public float rotationSpeed = 60f; 
+
+    void Update()
+    {
+        
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+    }
+}
